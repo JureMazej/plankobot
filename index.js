afa754bae58cd4c75cd53ab81c0655ffb2e6789e
JureMazej <jure.mazej2000@gmail.com>
2020-03-28 11:31:04 +0100

    f

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
+
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const token = 'NjkyODk4ODMxOTA5NTE5NDEy.Xn1qUA.KykIw4nOsdUm5Ro2_uifIRaA4ko';
 
-const token = 'NjkyODk4ODMxOTA5NTE5NDEy.Xn1pLA.Y8Wk1A0Y9EJNobGLaWmcAORBpmc';
+const translate = require('@vitalets/google-translate-api');
+translate.languages['sl'] = 'sl';
+
+
+function processFile(molitev){
+
+  	var randint = (Math.floor(Math.random() * 100)).toString();
+  	while (randint > 100){
+  		randint = (Math.floor(Math.random() * 100)).toString();
+  	}
+  	console.log(randint);
+  	var firstnumber = molitev.indexOf(randint);
+  	var nextnumber = molitev.indexOf((parseInt(randint) + 1).toString());
+
+  	var res = molitev.substring(firstnumber, nextnumber);
+  	
+  	return res;
+}
+
+
+
 
 bot.on('ready', () =>{
 	console.log('Online!');
 })
 
 bot.on('message', msg =>{
+
+
 	if(msg.content === "planko"){
 		msg.channel.send('Eminem je pssssssst!');
 	}
@@ -17,6 +41,23 @@ bot.on('message', msg =>{
 	else if(msg.content === "PLANKO"){
 		msg.channel.send('EMINEM JE PSSSSSSSSSSSSSSSSSST!!!!!');
 	}
+	else if(msg.content === "!moliteb"){
+		const fs = require('fs');
+		var molitev;  
+		fs.readFile('molitve.txt', (err, data) => { 
+	  		molitev = data.toString();
+		  	molitev = processFile(molitev);
+		  	translate(molitev, {to: 'sl'}).then(res => {
+   				msg.channel.send(res.text);
+			});
+		})
+	}
+
 })
+
+
+
+
+
 
 bot.login(token);
